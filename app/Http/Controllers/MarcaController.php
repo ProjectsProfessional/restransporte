4822e4e2466ddacfe0611a9840dbc4d05cde42da
ProjectsProfessional <projects.amorales@gmail.com>
2018-11-05 09:47:04 -0600

    Manejo de Controller de Marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -4,7 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Marca;
+
 class MarcaController extends Controller
 {
-    //
+	public function __construct()
+	{
+
+	}
+
+    public function create()
+    {
+    	$title = 'Definir Marca';
+    	return view('MarcaVehiculo.create',compact('title'));
+    }
+
+    public function store(request $request)
+    {
+    	$this->validate($request,[
+    		'code' => 'required',
+    		'description' =>'required',
+    	]);
+
+    	$data = request()->all();
+
+    	Marca::create([
+    		'ID_MARCA' =>$data['code'],
+    		'DESCRIPCION' =>$data['description'],
+    	]);
+    	return redirect()->route('marca');
+    }
+
 }
